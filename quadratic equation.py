d8a39f8fcfc2f02b36bef7ba2571d5b3594eb312
TwinlarryHover <42385641+TwinlarryHover@users.noreply.github.com>
2018-08-15 02:42:13 +0100

    Add files via upload
    Ajao Taye Hassan. 
    This is my first week assignment, though I got the message late to resume the class this Monday. Sir, kindly temper justice with mercy and award some mark as I tried my best to do the assignment even when I didn't start with them at all. 
    Thanks sir.

---
+++ quadratic equation.py
@@ -0,0 +1,14 @@
+#Solve Quadratic equation of x = (-b+4ac)/2a
+a = 2
+b = 3.5
+c = 5
+#Ask for the value of a
+int(input("What is a:\n"))
+#Ask for the value of b
+float(input("What is b:\n"))
+#Ask for the value of c
+int(input("What is c:\n"))
+x = (-b+4*a*c)/2*a
+print (x) 
+
+
